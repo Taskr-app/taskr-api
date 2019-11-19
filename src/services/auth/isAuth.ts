a1f3b8691f5fae994aa39bd4940bd8cca0865f0a
grantkang5 <g1k05j@gmail.com>
2019-11-19 13:31:29 -0800

    add isOwner middleware

--- src/services/auth/isAuth.ts
+++ src/services/auth/isAuth.ts
@@ -1,17 +1,20 @@
-import { MiddlewareFn } from 'type-graphql';
-import { MyContext } from '../context';
-import { verify } from 'jsonwebtoken';
-import { verifyIdToken } from './google';
-import { User } from '../../entity/User';
+import { MiddlewareFn } from "type-graphql";
+import { MyContext } from "../context";
+import { verify } from "jsonwebtoken";
+import { verifyIdToken } from "./google";
+import { User } from "../../entity/User";
+import { Team } from "../../entity/Team";
+import { Project } from "../../entity/Project";
+import { BaseEntity } from "typeorm";
 
 export const isAuth: MiddlewareFn<MyContext> = async ({ context }, next) => {
-  const authorization = context.req.headers['authorization'];
+  const authorization = context.req.headers["authorization"];
   if (!authorization) {
-    throw new Error('Not authenticated');
+    throw new Error("Not authenticated");
   }
 
   try {
-    const token = authorization.split(' ')[1];
+    const token = authorization.split(" ")[1];
     let payload = verify(token, process.env.ACCESS_TOKEN_SECRET!) as any;
     const { googleIdToken } = payload;
 
@@ -21,7 +24,7 @@ export const isAuth: MiddlewareFn<MyContext> = async ({ context }, next) => {
       if (googleTokenPayload) {
         const user = await User.findOne({ email: googleTokenPayload.email });
         if (!user) {
-          throw new Error('User not found');
+          throw new Error("User not found");
         }
         payload = { userId: user.id };
       }
@@ -31,5 +34,48 @@ export const isAuth: MiddlewareFn<MyContext> = async ({ context }, next) => {
     console.log(err);
     return err;
   }
+  return next();
+};
+
+type OwnerEntity = Team | Project;
+
+/**
+ *
+ * @param entity The entity used to the find the `owner` property. ie. Team // --> Team.owner
+ * @param idArg The argument name used to find the entity instance of the `owner` property. ie. "teamId" // --> Team.findOne({ where: { id: teamId } })
+ */
+
+export const isOwner: <T extends typeof BaseEntity>(
+  entity: T,
+  idArg: string
+) => MiddlewareFn<MyContext> = (entity, idArg) => async (
+  { context, args },
+  next
+) => {
+  try {
+    if (!context || !context.payload || !context.payload.userId) {
+      throw new Error(`Not authenticated`);
+    }
+    const id: string = args[idArg];
+    const entityClass = await entity.findOne({
+      relations: ["owner"],
+      where: { id }
+    });
+
+    if (!entityClass) throw new Error(`Entity not found`);
+    if (!(entityClass as OwnerEntity).owner) {
+      throw new Error(`Owner not found`) 
+    }
+
+    if (context.payload.userId !== (entityClass as OwnerEntity).owner.id) {
+      throw new Error(`You don't have permissions to access this request`);
+    }
+
+    context.entity = entityClass as OwnerEntity
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
+
   return next();
 };
