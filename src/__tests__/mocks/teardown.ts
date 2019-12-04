7b50ce1ec5101f7b031b4dd236ec4a75e49d2f9b
grantkang5 <g1k05j@gmail.com>
2019-12-03 16:32:06 -0800

    Create setup and teardown globals for jest

---
+++ src/__tests__/mocks/teardown.ts
@@ -0,0 +1,16 @@
+import 'dotenv/config';
+import { redis } from "../../services/redis";
+
+const globalTeardown = async () => {
+  try {
+    await redis.flushall();
+    await redis.disconnect();
+    console.log('Test environment has been shutdown successfully')
+    return true;
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
+}
+
+export default globalTeardown
