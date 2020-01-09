31832fd8456c75642f45fdcf99e2d145ebe3be19
jiminykbob <jikang92@hotmail.com>
2020-01-09 11:47:23 -0800

    update project based resolvers

--- src/resolvers/TaskResolver.ts
+++ src/resolvers/TaskResolver.ts
@@ -9,34 +9,39 @@ import {
   Root,
   Publisher,
   Int,
-  Query
-} from "type-graphql";
-import { Task } from "../entity/Task";
-import { List } from "../entity/List";
-import { isAuth } from "./middleware";
-import { User } from "../entity/User";
-import { uniqBy } from "lodash";
+  Query,
+  PubSubEngine
+} from 'type-graphql';
+import { Task } from '../entity/Task';
+import { List } from '../entity/List';
+import { isAuth } from './middleware';
+import { User } from '../entity/User';
+import { uniqBy } from 'lodash';
+import { createQueryBuilder } from 'typeorm';
 
 const topics = {
-  create: "CREATE_TASK",
-  update: "UPDATE_TASK",
-  delete: "DELETE_TASK",
-  addMember: "ADD_TASK_MEMBER",
-  removeMember: "REMOVE_TASK_MEMBER"
-}
+  create: 'CREATE_TASK',
+  update: 'UPDATE_TASK',
+  delete: 'DELETE_TASK',
+  addMember: 'ADD_TASK_MEMBER',
+  removeMember: 'REMOVE_TASK_MEMBER',
+  move: 'MOVE_TASK'
+};
+
+const buffer = 16384;
 
 @Resolver()
 export class TaskResolver {
   @Query(() => [Task])
   @UseMiddleware(isAuth)
-  async getListTasks(@Arg("listId", () => ID) listId: number) {
+  async getListTasks(@Arg('listId', () => ID) listId: number) {
     try {
       const list = await List.findOne({
-        relations: ["tasks"],
+        relations: ['tasks'],
         where: { id: listId }
       });
       if (!list) throw new Error(`This list doesn't exist`);
-      return list.tasks
+      return list.tasks;
     } catch (err) {
       console.log(err);
       return err;
@@ -47,9 +52,9 @@ export class TaskResolver {
   @UseMiddleware(isAuth)
   async createTask(
     @PubSub(topics.create) publish: Publisher<Task>,
-    @Arg("listId", () => ID) listId: number,
-    @Arg("name") name: string,
-    @Arg("desc", { nullable: true }) desc?: string
+    @Arg('listId', () => ID) listId: string,
+    @Arg('name') name: string,
+    @Arg('desc', { nullable: true }) desc?: string
   ) {
     try {
       const list = await List.findOne({
@@ -75,28 +80,110 @@ export class TaskResolver {
   @UseMiddleware(isAuth)
   async updateTask(
     @PubSub(topics.update) publish: Publisher<Task>,
-    @Arg("id", () => ID) id: number,
-    @Arg("listId", () => ID, { nullable: true }) listId: number,
-    @Arg("name", { nullable: true }) name: string,
-    @Arg("desc", { nullable: true }) desc: string,
-    @Arg("dueDate", { nullable: true }) dueDate: Date
+    @Arg('id', () => ID) id: number,
+    @Arg('listId', () => ID, { nullable: true }) listId: number,
+    @Arg('name', { nullable: true }) name: string,
+    @Arg('desc', { nullable: true }) desc: string,
+    @Arg('dueDate', { nullable: true }) dueDate: Date
   ) {
     try {
-      const task = await Task.findOne({ where: { id } })
-      if (!task) throw new Error(`This task doesn't exist`)
+      const task = await Task.findOne({ where: { id } });
+      if (!task) throw new Error(`This task doesn't exist`);
       if (listId) {
-        const list = await List.findOne({ where: { id: listId } })
-        if (!list) throw new Error(`This list doesn't exist`)
-        task.list = list
+        const list = await List.findOne({ where: { id: listId } });
+        if (!list) throw new Error(`This list doesn't exist`);
+        task.list = list;
       }
-      task.name = name ? name : task.name
-      task.desc = desc ? desc : task.desc
-      task.dueDate = dueDate ? dueDate : task.dueDate
-      const newTask = await task.save()
-      await publish(newTask)
-      return newTask
+      task.name = name ? name : task.name;
+      task.desc = desc ? desc : task.desc;
+      task.dueDate = dueDate ? dueDate : task.dueDate;
+      const newTask = await task.save();
+      await publish(newTask);
+      return newTask;
     } catch (err) {
-      console.log(err)
+      console.log(err);
+      return err;
+    }
+  }
+
+  @Mutation(() => Boolean)
+  @UseMiddleware(isAuth)
+  async updateTaskPos(
+    @PubSub() pubSub: PubSubEngine,
+    @Arg('id', () => ID) id: string,
+    @Arg('listId', () => ID, { nullable: true }) listId?: string,
+    @Arg('aboveId', () => ID, { nullable: true }) aboveId?: string,
+    @Arg('belowId', () => ID, { nullable: true }) belowId?: string
+  ) {
+    try {
+      const targetTask = await Task.findOne({
+        relations: ['list'],
+        where: { id }
+      });
+      if (!targetTask) {
+        throw new Error('Task does not exist');
+      }
+
+      if (listId) {
+        const newList = await List.findOne({
+          relations: ['tasks'],
+          where: { id: listId }
+        });
+        if (!newList) {
+          throw new Error('List does not exist');
+        }
+        targetTask.list = newList;
+      }
+
+      // move target to bottom of list or to a list with no tasks
+      if (belowId === undefined) {
+        targetTask.pos = targetTask.list.maxPos + buffer;
+        targetTask.list.maxPos = targetTask.pos;
+      }
+
+      // move target to top of list
+      else if (aboveId === undefined) {
+        targetTask.list.tasks = await createQueryBuilder(Task, 'task')
+          .where(`"task"."listId" = :id`, { id: targetTask.list.id })
+          .getMany();
+        // get pos of first task
+        const firstTask = targetTask.list.tasks.find(task => {
+          return task.id === parseInt(belowId);
+        });
+        if (!firstTask) {
+          throw new Error('First task does not exist');
+        }
+        targetTask.pos = firstTask.pos / 2;
+      }
+
+      // move target between aboveTask and belowTask
+      else {
+        targetTask.list.tasks = await createQueryBuilder(Task, 'task')
+          .where(`"task"."listId" = :id`, { id: targetTask.list.id })
+          .getMany();
+        const aboveTask = targetTask.list.tasks.find(
+          task => task.id === parseInt(aboveId!)
+        );
+        if (!aboveTask) {
+          throw new Error('Task above does not exist');
+        }
+        const belowTask = targetTask.list.tasks.find(
+          task => task.id === parseInt(belowId!)
+        );
+        if (!belowTask) {
+          throw new Error('Task below does not exist');
+        }
+
+        targetTask.pos = (aboveTask.pos + belowTask.pos) / 2;
+      }
+      // TODO check if pos numbers get too close to each other .0001 apart or smth
+      // renumber the cards and nearby cards
+
+      await targetTask.save();
+      await pubSub.publish(topics.move, targetTask);
+      return true;
+    } catch (err) {
+      console.log(err);
       return err;
     }
   }
@@ -105,14 +192,17 @@ export class TaskResolver {
   @UseMiddleware(isAuth)
   async deleteTask(
     @PubSub(topics.delete) publish: Publisher<Task>,
-    @Arg("taskId", () => ID) taskId: number
+    @Arg('taskId', () => ID) taskId: number
   ) {
     try {
-      const task = await Task.findOne({ where: { id: taskId } })
-      if (!task) throw new Error(`This task doesn't exist`)
-      await publish(task)
-      await task.remove()
-      return true
+      const task = await Task.findOne({
+        where: { id: taskId },
+        relations: ['list']
+      });
+      if (!task) throw new Error(`This task doesn't exist`);
+      await publish(task);
+      await task.remove();
+      return true;
     } catch (err) {
       console.log(err);
       return err;
@@ -123,20 +213,20 @@ export class TaskResolver {
   @UseMiddleware(isAuth)
   async addTaskMember(
     @PubSub(topics.addMember) publish: Publisher<Task>,
-    @Arg("id", () => ID) id: number,
+    @Arg('id', () => ID) id: number,
     @Arg('userId', () => ID) userId: number
   ) {
     try {
-      const task = await Task.findOne({ where: { id } })
-      if (!task) throw new Error(`This task doesn't exist`)
-      const user = await User.findOne({ where: { id: userId } })
-      if (!user) throw new Error(`This user doesn't exist`)
+      const task = await Task.findOne({ where: { id } });
+      if (!task) throw new Error(`This task doesn't exist`);
+      const user = await User.findOne({ where: { id: userId } });
+      if (!user) throw new Error(`This user doesn't exist`);
       task.users = uniqBy([...task.users, user], 'id');
       await publish(task);
       await task.save();
       return true;
     } catch (err) {
-      console.log(err)
+      console.log(err);
       return err;
     }
   }
@@ -149,12 +239,12 @@ export class TaskResolver {
     @Arg('userId', () => ID) userId: string
   ) {
     try {
-      const task = await Task.findOne({ where: { id } })
-      if (!task) throw new Error(`This task doesn't exist`)
-      const user = await User.findOne({ where: { id: userId } })
+      const task = await Task.findOne({ where: { id } });
+      if (!task) throw new Error(`This task doesn't exist`);
+      const user = await User.findOne({ where: { id: userId } });
       if (!user) throw new Error(`This user doesn't exist`);
-      task.users = task.users.filter(user => user.id !== parseInt(userId))
-      await publish(task)
+      task.users = task.users.filter(user => user.id !== parseInt(userId));
+      await publish(task);
       await task.save();
       return true;
     } catch (err) {
@@ -165,10 +255,21 @@ export class TaskResolver {
 
   @Subscription(() => Task, {
     topics: topics.create,
-    filter: ({ payload, args }) => args.listId === payload.list.id
+    filter: ({ payload, args }) => parseInt(args.listId) === payload.list.id
   })
-  newTask(@Root() task: Task, @Arg("listId", () => Int) _listId: number) {
+  onTaskCreated(@Root() task: Task, @Arg('listId', () => ID) _listId: string) {
     return task;
+  }
+
+  @Subscription(() => Task, {
+    topics: topics.delete,
+    filter: ({ payload, args }) => parseInt(args.listId) === payload.list.id
+  })
+  onTaskDeleted(
+    @Root() deletedTask: Task,
+    @Arg('listId', () => ID) _taskId: string
+  ) {
+    return deletedTask;
   }
 
   @Subscription(() => Task, {
@@ -177,23 +278,31 @@ export class TaskResolver {
   })
   updatedTask(
     @Root() updatedTask: Task,
-    @Arg("taskId", () => Int) _taskId: number
+    @Arg('taskId', () => Int) _taskId: number
   ) {
-    return updatedTask
+    return updatedTask;
   }
 
-  @Subscription(() => Task, {
-    topics: topics.delete,
+  @Subscription({
+    topics: topics.move,
     filter: ({ payload, args }) => {
-      console.log(payload, args)
-      return args.taskId === payload.id
+      try {
+        if (!payload.list) {
+          throw new Error('payload.list doesnt exist');
+        }
+        return payload.list.id === parseInt(args.listId);
+      } catch (err) {
+        console.log(err);
+        return false;
+      }
     }
   })
-  deletedTask(
-    @Root() deletedTask: Task,
-    @Arg("taskId", () => Int) _taskId: number
-  ) {
-    return deletedTask
+  onTaskMoved(@Root() task: Task, @Arg('listId', () => ID) _: string): Task {
+    try {
+      return task;
+    } catch (err) {
+      throw new Error('ontaskmoved fail');
+    }
   }
 
   @Subscription(() => Task, {
@@ -202,9 +311,9 @@ export class TaskResolver {
   })
   addedTaskMember(
     @Root() addedTaskMember: Task,
-    @Arg("taskId", () => Int) _taskId: number
+    @Arg('taskId', () => Int) _taskId: number
   ) {
-    return addedTaskMember
+    return addedTaskMember;
   }
 
   @Subscription(() => Task, {
@@ -213,8 +322,8 @@ export class TaskResolver {
   })
   removedTaskMember(
     @Root() removeTaskMember: Task,
-    @Arg("taskId", () => Int) _taskId: number
+    @Arg('taskId', () => Int) _taskId: number
   ) {
-    return removeTaskMember
+    return removeTaskMember;
   }
 }
--- src/resolvers/middleware/isAuth.ts
+++ src/resolvers/middleware/isAuth.ts
@@ -1,17 +1,17 @@
-import { MiddlewareFn } from "type-graphql";
-import { MyContext } from "../../services/context";
-import { verify } from "jsonwebtoken";
-import { verifyIdToken } from "./../../services/auth/google";
-import { User } from "../../entity/User";
+import { MiddlewareFn } from 'type-graphql';
+import { MyContext } from '../../services/context';
+import { verify } from 'jsonwebtoken';
+import { verifyIdToken } from './../../services/auth/google';
+import { User } from '../../entity/User';
 
 export const isAuth: MiddlewareFn<MyContext> = async ({ context }, next) => {
-  const authorization = context.req.headers["authorization"];
+  const authorization = context.req.headers['authorization'];
   if (!authorization) {
-    throw new Error("Not authenticated");
+    throw new Error('Not authenticated');
   }
 
   try {
-    const token = authorization.split(" ")[1];
+    const token = authorization.split(' ')[1];
     let payload = verify(token, process.env.ACCESS_TOKEN_SECRET!) as any;
     const { googleIdToken } = payload;
 
@@ -21,7 +21,7 @@ export const isAuth: MiddlewareFn<MyContext> = async ({ context }, next) => {
       if (googleTokenPayload) {
         const user = await User.findOne({ email: googleTokenPayload.email });
         if (!user) {
-          throw new Error("User not found");
+          throw new Error('User not found');
         }
         payload = { userId: user.id };
       }
