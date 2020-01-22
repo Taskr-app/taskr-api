00e35db2388c9be91926c58fc581f533ccbe30d5
grantkang5 <g1k05j@gmail.com>
2020-01-22 15:28:18 -0800

    rearrange test logs for accuracy

--- src/__tests__/mocks/setup.ts
+++ src/__tests__/mocks/setup.ts
@@ -7,10 +7,11 @@ const globalSetup = async () => {
   console.log('\x1b[2m%s\x1b[0m', 'Applying global test setup...');
   try {
     const { stdout, stderr } = await exec('yarn db:seed');
-    await redis.flushall();
     if (stdout) console.log('Running seed script: ', stdout);
     if (stderr) console.log('Seed error: ', stderr);
     console.log('\x1b[32m%s\x1b[0m', 'Successfully seeded test database');
+    await redis.flushall();
+    console.log('\x1b[2m%s\x1b[0m', 'Redis storage reset')
     return true;
   } catch (err) {
     console.log(err);
--- src/__tests__/mocks/teardown.ts
+++ src/__tests__/mocks/teardown.ts
@@ -4,6 +4,7 @@ import { redis } from '../../services/redis';
 const globalTeardown = async () => {
   try {
     await redis.flushall();
+    console.log('\x1b[2m%s\x1b[0m', 'Redis storage reset')
     redis.disconnect();
     console.log('Test environment has been shutdown successfully');
     return true;
