b1821051db3eed3649cedd59c47ce8138f8ea7e4
grantkang5 <g1k05j@gmail.com>
2020-01-22 15:30:04 -0800

    change project-invite email queries for link

--- src/services/emails/projectInviteEmail.ts
+++ src/services/emails/projectInviteEmail.ts
@@ -1,10 +1,11 @@
 import { fromEmail } from './transporter';
 import { emailTemplate } from './template';
+import { Project } from '../../entity/Project';
 
 interface emailArgs {
   sender: string;
   email: string;
-  projectName: string;
+  project: Partial<Project>;
   link: string;
   message?: string;
 }
@@ -12,7 +13,7 @@ interface emailArgs {
 export const projectInviteEmail = ({
   sender,
   email,
-  projectName,
+  project,
   link,
   message
 }: emailArgs) => {
@@ -21,11 +22,11 @@ export const projectInviteEmail = ({
     to: email,
     subject: 'Project Invite | Taskr',
     html: emailTemplate({
-      header: `${sender} has sent you a project invite to ${projectName}`,
+      header: `${sender} has sent you a project invite to ${project.name}`,
       message,
-      body: `You've been invited as a project member to ${projectName}. You will have access to all tasks and messages shared in the project.`,
+      body: `You've been invited as a project member to ${project.name}. You will have access to all tasks and messages shared in the project.`,
       cta: 'Accept project invitation',
-      link: `${process.env.CLIENT_URL}/invite/project/success?email=${email}&id=${link}`,
+      link: `${process.env.CLIENT_URL}/invite/project/success?email=${email}&id=${project.id}&link=${link}`,
       footer: 'This email will be invalid after 2 hours from being sent'
     })
   };
