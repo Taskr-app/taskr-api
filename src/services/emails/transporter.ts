ac564bc278b3a8c0131d18821aa0b8eb233b9a33
grantkang5 <g1k05j@gmail.com>
2020-01-22 15:29:37 -0800

    create stub for test-environment

--- src/services/emails/transporter.ts
+++ src/services/emails/transporter.ts
@@ -1,15 +1,19 @@
 import nodemailer from 'nodemailer';
+import { stubTransport } from 'nodemailer-stub'
 
-const transport =
-  process.env.NODE_ENV === 'production'
-    ? {
+const transport = () => {
+  switch(process.env.NODE_ENV) {
+    case 'production':
+      return {
         service: 'gmail',
         auth: {
           user: process.env.MAILER_EMAIL,
           pass: process.env.MAILER_PASSWORD
         }
-      }
-    : {
+      };
+
+    case 'development':
+      return {
         host: 'smtp.ethereal.email',
         port: 587,
         auth: {
@@ -18,5 +22,20 @@ const transport =
         }
       };
 
-export const transporter = nodemailer.createTransport(transport);
+    case 'test':
+      return stubTransport
+
+    default:
+      return {
+        host: 'smtp.ethereal.email',
+        port: 587,
+        auth: {
+          user: process.env.MAILER_EMAIL,
+          pass: process.env.MAILER_PASSWORD
+        }
+      };
+  }
+}
+
+export const transporter = nodemailer.createTransport(transport());
 export const fromEmail = `Do Not Reply <${process.env.MAILER_EMAIL}>`;
